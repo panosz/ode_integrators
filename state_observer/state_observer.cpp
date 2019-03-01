d75d58b6c7d60b685a177d5f327feeaf494a6a3d
Panagiotis Zestanakis <panosz@gmail.com>
2019-03-01 19:31:36 +0200

    moved StateWrapper to separate compilation unit

--- state_observer/state_observer.cpp
+++ state_observer/state_observer.cpp
@@ -5,17 +5,11 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-#include <vector>
-#include <stdexcept>
-#include <utility>
 
 #include <cmath>
 
-#include <boost/operators.hpp>
-#include <boost/range/algorithm/max_element.hpp>
-
-#include "general_file_io.hpp"
 #include "orbit_classes.hpp"
+#include "state_wrapper.hpp"
 #include "state_specific_file_io.hpp"
 #include "system_and_poincare_surface.hpp"
 #include "integration_utilities.hpp"
@@ -25,110 +19,7 @@
 /* The type of container used to hold the state vector */
 namespace DS
 {
-    class StateWrapper : boost::additive<StateWrapper, boost::additive<StateWrapper, double,
-        boost::multiplicative<StateWrapper, double> > > {
-      using T=std::array<double, 3>;
-     private:
-      T s_{0, 0, 0};
 
-     public:
-      using value_type = typename T::value_type;
-      using iterator =  typename T::iterator;
-      using const_iterator = typename T::const_iterator;
-
-      StateWrapper (value_type x, value_type y, value_type z)
-          : s_{x, y, z}
-      { };
-      StateWrapper ()
-          : s_{{0, 0, 0}}
-      { };
-
-      value_type& operator[] (unsigned i)
-      { return s_[i]; };
-
-      value_type operator[] (unsigned i) const
-      { return s_[i]; };
-
-      auto begin ()
-      {
-        return std::begin(s_);
-      }
-
-      auto begin () const
-      {
-        return std::cbegin(s_);
-      }
-
-      auto end ()
-      {
-        return std::end(s_);
-      }
-
-      auto end () const
-      {
-        return std::cend(s_);
-      }
-
-      StateWrapper& operator+= (double d)
-      {
-        for (auto& x: s_)
-          x += d;
-        return *this;
-      }
-
-      StateWrapper& operator*= (double d)
-      {
-        for (auto& x: s_)
-          x *= d;
-        return *this;
-      }
-
-      StateWrapper& operator+= (const StateWrapper& other)
-      {
-        for (int i = 0; i < std::size(s_); ++i)
-          s_[i] += other.s_[i];
-        return *this;
-      }
-
-      StateWrapper operator/ (const StateWrapper& other) const
-      {
-        StateWrapper output{*this};
-        for (unsigned long i = 0; i < std::size(s_); ++i)
-          output.s_[i] /= other.s_[i];
-        return output;
-      }
-
-      StateWrapper abs () const
-      {
-        StateWrapper output{*this};
-        for (auto& x: output.s_)
-          x = std::abs(x);
-        return output;
-      }
-
-      double norm_inf () const
-      {
-        const auto s_abs = abs();
-        auto max_it = boost::range::max_element(s_abs.s_);
-        return *max_it;
-      }
-
-    };
-
-    StateWrapper abs (const StateWrapper& s)
-    {
-      return s.abs();
-    }
-
-
-
-    std::ostream& operator<< (std::ostream& os, const StateWrapper& state)
-    {
-      {
-        boost::copy(state, std::ostream_iterator<typename StateWrapper::value_type>(os, " "));
-        return os;
-      }
-    }
 
     class ExtendedHarmonicOscillator {
      public:
@@ -150,17 +41,7 @@ namespace DS
 }
 //void write_to_files(const char* filename_)
 
-namespace boost { namespace numeric { namespace odeint {
-            template<>
-            struct vector_space_norm_inf< DS::StateWrapper >
-            {
-                typedef double result_type;
-                double operator()( const DS::StateWrapper &s ) const
-                {
-                  return s.norm_inf();
-                }
-            };
-        } } }
+
 
 
 int main (int argc, char *argv[])
