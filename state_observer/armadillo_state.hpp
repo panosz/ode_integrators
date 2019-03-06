a0110d38cca854c6121f9e88763c9c8224c9c1be
Panagiotis Zestanakis <panosz@gmail.com>
2019-03-06 14:00:05 +0200

    Corrected loss of precision when saving armadillo_state to text
    Operator << for armadillo sacrifices specified precision in favour of beauty
    Use method raw_print instead

--- state_observer/armadillo_state.hpp
+++ state_observer/armadillo_state.hpp
@@ -6,7 +6,7 @@
 #define ODE_INTEGRATORS_ARMADILLO_STATE_HPP
 #include <armadillo>
 #include <boost/numeric/odeint/algebra/vector_space_algebra.hpp>
-
+#include "orbit_classes.hpp"
 
 namespace DS
 {
@@ -14,19 +14,32 @@ namespace DS
 
 }
 
-
-namespace boost { namespace numeric { namespace odeint {
+namespace boost
+{
+    namespace numeric
+    {
+        namespace odeint
+        {
             template<>
-            struct vector_space_norm_inf< DS::armadillo_state >
-            {
+            struct vector_space_norm_inf<DS::armadillo_state> {
                 typedef double result_type;
-                double operator()( const DS::armadillo_state &p ) const
+                double operator() (const DS::armadillo_state& p) const
                 {
-                  return arma::norm(p,"inf");
+                  return arma::norm(p, "inf");
                 }
             };
-        } } }
+        }
+    }
+}
 
+std::ostream& operator<< (std::ostream& os, const std::vector<DS::armadillo_state>& states)
+{
+  {
 
+    for (const auto& s : states)
+      s.raw_print(os);
+    return os;
+  }
+}
 
 #endif //ODE_INTEGRATORS_ARMADILLO_STATE_HPP
