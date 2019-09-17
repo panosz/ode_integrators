64a58f8ec10a7d87e730fd7453ce1a4f6caa9b65
Panagiotis Zestanakis <panosz@gmail.com>
2019-09-17 17:44:07 +0300

    expose ActionIntegrationResult to python

--- poincare_map/bindings/src/hello_ext.cpp
+++ poincare_map/bindings/src/hello_ext.cpp
@@ -1,6 +1,7 @@
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
 #include <string>
+#include "action_integration_result.hpp"
 
 namespace p = boost::python;
 namespace np = boost::python::numpy;
@@ -92,6 +93,51 @@ namespace { // Avoid cluttering the global namespace.
 
 
 
+class ActionIntegrationResultDecorator
+{
+  private:
+    ActionIntegrationResult air_;
+  public:
+    ActionIntegrationResultDecorator(const ActionIntegrationResult& air):
+      air_{air}{};
+    ActionIntegrationResultDecorator(double x, double y, double z, SpecialIntegrals si):
+      air_{x,y,z,si}{};
+    double Action () const noexcept
+    {return air_.Action();}
+    double theta_period () const noexcept
+    {return air_.theta_period();}
+    double delta_phi () const noexcept
+    {return air_.delta_phi();}
+    double omega_theta () const
+    {return air_.omega_theta();}
+    double g_factor () const noexcept
+    {return air_.g_factor();}
+    double omega_phi () const
+    {return air_.omega_phi();}
+    double domega_dJ () const
+    {return air_.domega_dJ();}
+    double d2K_dJ2 () const
+    {return air_.d2K_dJ2();}
+    double one_div_two_pi_gamma () const
+    {return air_.one_div_two_pi_gamma();}
+    double domega_dF () const
+    {return air_.domega_dF();}
+    double d2K_dJdF () const
+    {return air_.d2K_dJdF();}
+    double d2K_dF2 () const
+    {return air_.d2K_dF2();}
+    np::ndarray hessian() const
+    {return args_to_Hessian(air_.d2K_dJ2(), air_.d2K_dJdF(), air_.d2K_dF2());}
+
+};
+
+
+ActionIntegrationResultDecorator make_action_integration_result()
+{
+  return ActionIntegrationResultDecorator(1, 2, 3, SpecialIntegrals());
+}
+
+
 BOOST_PYTHON_MODULE(hello_ext)
 {
   np::initialize();  // have to put this in any module that uses Boost.NumPy
@@ -107,5 +153,22 @@ BOOST_PYTHON_MODULE(hello_ext)
     .def_readwrite("y",&MyPoint::y_) ;
   //exposed factory functions, are python factory functions for the exposed python class
   p::def("make_myPoint",make_myPoint);
+
+  p::class_<ActionIntegrationResultDecorator>("ActionIntegrationResult", p::init<double,double,double,SpecialIntegrals>())
+    .def("Action",&ActionIntegrationResultDecorator::Action)
+    .def("theta_period",&ActionIntegrationResultDecorator::theta_period)
+    .def("delta_phi",&ActionIntegrationResultDecorator::delta_phi)
+    .def("omega_theta",&ActionIntegrationResultDecorator::omega_theta)
+    .def("g_factor",&ActionIntegrationResultDecorator::g_factor)
+    .def("omega_phi",&ActionIntegrationResultDecorator::omega_phi)
+    .def("domega_dJ",&ActionIntegrationResultDecorator::domega_dJ)
+    .def("d2K_dJ2",&ActionIntegrationResultDecorator::d2K_dJ2)
+    .def("one_div_two_pi_gamma",&ActionIntegrationResultDecorator::one_div_two_pi_gamma)
+    .def("domega_dF",&ActionIntegrationResultDecorator::domega_dF)
+    .def("d2K_dJdF",&ActionIntegrationResultDecorator::d2K_dJdF)
+    .def("d2K_dF2",&ActionIntegrationResultDecorator::d2K_dF2)
+    .def("hessian",&ActionIntegrationResultDecorator::hessian);
+
+  p::def("make_action_integration_result",make_action_integration_result);
 }
 
