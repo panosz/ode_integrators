a8222fbb7fa99fbaad838e91adee03e321e6558a
Panagiotis Zestanakis <panosz@gmail.com>
2019-10-09 22:22:36 +0300

    Refactoring integration_utilities.hpp
    --mainly stylistic changes, such as breaking long lines.
    
    -- also extracting const variables, when passed as arguments, to improve
        readability.

--- poincare_map/include/integration_utilities.hpp
+++ poincare_map/include/integration_utilities.hpp
@@ -35,7 +35,9 @@ struct IntegrationOptions
     double rel_err;
     double dt;
 
-    IntegrationOptions (double abs_error, double rel_error, double init_time_step)
+    IntegrationOptions (double abs_error,
+                        double rel_error,
+                        double init_time_step)
         : abs_err{abs_error}, rel_err{rel_error}, dt{init_time_step}
     { }
 };
@@ -57,7 +59,12 @@ typename System::StateType step_on_surface(SystemAndPoincareSurface<System> sys,
   };
 
   const double distance_from_surface = -sys.surface_eval(s);
-  stepper.do_step(systemPerpendicularToSurface, s, 0, state_on_surface, distance_from_surface);
+  stepper.do_step(systemPerpendicularToSurface,
+                  s,
+                  0,
+                  state_on_surface,
+                  distance_from_surface);
+
   return state_on_surface;
 }
 
@@ -67,7 +74,7 @@ auto make_OrbitRange (System sys,
                       typename System::StateType& init_state,
                       double integration_time,
                       IntegrationOptions options)
-/// \brief returnded orbit has range of type addaptive_range
+/// \brief returned orbit has range of type adaptive_range
 /// \tparam System
 /// \param sys
 /// \param init_state
@@ -77,13 +84,18 @@ auto make_OrbitRange (System sys,
 {
   const double integration_start_time = 0;
 
-  const auto controlled_stepper = make_controlled(options.abs_err, options.rel_err, ErrorStepperType<System>());
+  const auto controlled_stepper = make_controlled(options.abs_err,
+                                                  options.rel_err,
+                                                  ErrorStepperType<System>());
 
   auto orbit_iterators = make_adaptive_range(controlled_stepper,
                                              sys,
-                                             init_state, integration_start_time, integration_time, options.dt);
+                                             init_state,
+                                             integration_start_time,
+                                             integration_time, options.dt);
 
-  return boost::make_iterator_range(orbit_iterators.first, orbit_iterators.second);
+  return boost::make_iterator_range(orbit_iterators.first,
+                                    orbit_iterators.second);
 }
 
 template<typename System>
@@ -91,18 +103,24 @@ auto make_OrbitTimeRange (System sys,
                           typename System::StateType& init_state,
                           double integration_time,
                           IntegrationOptions options)
-///returned orbit has range of type addaptive_time_range
+///returned orbit has range of type adaptive_time_range
 
 {
   const double integration_start_time = 0;
 
-  const auto controlled_stepper = make_controlled(options.abs_err, options.rel_err, ErrorStepperType<System>());
+  const auto controlled_stepper = make_controlled(options.abs_err,
+                                                  options.rel_err,
+                                                  ErrorStepperType<System>());
 
   auto orbit_iterators = make_adaptive_time_range(controlled_stepper,
                                                   sys,
-                                                  init_state, integration_start_time, integration_time, options.dt);
+                                                  init_state,
+                                                  integration_start_time,
+                                                  integration_time,
+                                                  options.dt);
 
-  return boost::make_iterator_range(orbit_iterators.first, orbit_iterators.second);
+  return boost::make_iterator_range(orbit_iterators.first,
+                                    orbit_iterators.second);
 }
 
 template<typename System>
@@ -117,12 +135,15 @@ class ParticleOrbit {
   mutable StateType state_;
  public:
 
-  ParticleOrbit (System sys, StateType init_state, double integration_time,
+  ParticleOrbit (System sys,
+                 StateType init_state,
+                 double integration_time,
                  IntegrationOptions options)
-      : sys_{sys}, init_state_{init_state},
-        integration_time_{integration_time},
-        options_{options},
-        state_(init_state)
+               : sys_{sys},
+                 init_state_{init_state},
+                 integration_time_{integration_time},
+                 options_{options},
+                 state_(init_state)
   { }
 
   StateType init_state () const
@@ -135,14 +156,20 @@ class ParticleOrbit {
   }
 
   auto range ()
-  /// should not follow more than two instances of the same orbit range simultaneously
+  /// should not follow more than two instances of the same orbit range
+  /// simultaneously
   {
     state_ = init_state_;
-    return make_OrbitRange(sys_, state_, integration_time_, options_);
+
+    return make_OrbitRange(sys_,
+                           state_,
+                           integration_time_,
+                           options_);
   }
 
   auto time_range ()
-  /// should not follow more than two instances of the same orbit range simultaneously
+  /// should not follow more than two instances of the same orbit range
+  /// simultaneously
   {
     state_ = init_state_;
     return make_OrbitTimeRange(sys_, state_, integration_time_, options_);
@@ -150,12 +177,16 @@ class ParticleOrbit {
 
 };
 
+
 template<typename System>
-auto make_ParticleOrbit (System sys, typename System::StateType init_state, double integration_time,
+auto make_ParticleOrbit (System sys,
+                         typename System::StateType init_state,
+                         double integration_time,
                          IntegrationOptions options)
 {
   return ParticleOrbit<System>(sys, init_state, integration_time, options);
 }
+
 
 template<typename OrbitType>
 std::vector<typename OrbitType::StateType>
@@ -170,24 +201,32 @@ rough_cross_points (OrbitType& orbit, Surface surface)
 
   std::cout << "start following orbit" << std::endl;
 
-  PanosUtilities::zero_cross_transformed(orbit.range(), std::back_inserter(output),
-                                         surface_fun, MAX_SURFACE_CROSS_DISTANCE, surface.direction);
+  PanosUtilities::zero_cross_transformed(orbit.range(),
+                                         std::back_inserter(output),
+                                         surface_fun,
+                                         MAX_SURFACE_CROSS_DISTANCE,
+                                         surface.direction);
 
   std::cout << "calculated " << output.size() << " cross points" << std::endl;
   return output;
 }
 
+
 template<typename System>
 typename System::StateType
-accurate_from_rough_cross_point (SystemAndPoincareSurface<System> sys, const typename System::StateType& rough_cross_point)
+accurate_from_rough_cross_point (SystemAndPoincareSurface<System> sys,
+                                 const typename System::StateType&
+                                                       rough_cross_point)
 {
   return step_on_surface(sys, rough_cross_point, ErrorStepperType<System>());
 }
 
+
 template<typename System>
 std::vector<typename System::StateType>
 accurate_from_rough_cross_points (SystemAndPoincareSurface<System> sys,
-                                  const std::vector<typename System::StateType>& rough_cross_input)
+                                  const std::vector<typename System::StateType>&
+                                                        rough_cross_input)
 {
   std::vector<typename System::StateType> fine_cross_output{};
 
@@ -207,12 +246,17 @@ trace_on_poincare_surface (SystemAndPoincareSurface<System> sys_and_pc,
                            double integration_time,
                            IntegrationOptions options)
 {
-  auto orbit = make_ParticleOrbit(sys_and_pc, init_state, integration_time, options);
+  auto orbit = make_ParticleOrbit(sys_and_pc,
+                                  init_state,
+                                  integration_time,
+                                  options);
 
-  const auto approximate_points = rough_cross_points(orbit,
-                                                     sys_and_pc.poincare_surface());
+  const auto approximate_points =
+                rough_cross_points(orbit,
+                                   sys_and_pc.poincare_surface());
 
-  auto cross_points = accurate_from_rough_cross_points(sys_and_pc, approximate_points);
+  auto cross_points = accurate_from_rough_cross_points(sys_and_pc,
+                                                       approximate_points);
 
   return make_OrbitCrossOutput(init_state, cross_points);
 }
@@ -227,7 +271,12 @@ trace_on_poincare_surface (SystemAndPoincareSurface<System> sys_and_pc,
   std::vector<OrbitCrossOutput> output;
 
   for (const auto& state: init_states)
-    output.push_back(trace_on_poincare_surface(sys_and_pc, state, integration_time, options));
+  {
+    output.push_back(trace_on_poincare_surface(sys_and_pc,
+                                               state,
+                                               integration_time,
+                                               options));
+  }
 
   return output;
 
@@ -250,15 +299,22 @@ namespace
 
 
     template<typename System>
-    auto system_and_poincare_surface_for_closed_orbit_integration (System sys, const typename System::StateType& init_state)
+    auto system_and_poincare_surface_for_closed_orbit_integration
+                            (System sys,
+                             const typename System::StateType& init_state)
     {
-      const auto zero_cross_position = init_state[static_cast<unsigned >(CoordinateTag::q)];
+      const auto zero_cross_position =
+         init_state[static_cast<unsigned >(CoordinateTag::q)];
+
       typename System::StateType init_derivatives{};
+
       sys(init_state, init_derivatives, 0);
 
-      const auto init_dqdt = init_derivatives[static_cast<unsigned >(CoordinateTag::q)];
+      const auto init_dqdt =
+        init_derivatives[static_cast<unsigned >(CoordinateTag::q)];
 
-      const int zero_cross_direction = (init_dqdt > 0) - (init_dqdt < 0); //direction is the sign of dqdt
+      //direction is the sign of dqdt
+      const int zero_cross_direction = (init_dqdt > 0) - (init_dqdt < 0);
 
 
       auto my_poincare_surface = Surface{CoordinateTag::q,
@@ -272,24 +328,35 @@ namespace
     template<typename System, typename OutputContainer>
     auto
     integrate_along_closed_orbit_impl (System sys,
-                                       const typename System::StateType& first_point,
+                                       const typename System::StateType&
+                                                       first_point,
                                        OutputContainer& outputContainer,
                                        double max_time,
                                        IntegrationOptions integrationOptions)
     {
-      auto my_system_and_pc = system_and_poincare_surface_for_closed_orbit_integration(sys, first_point);
+      auto my_system_and_pc =
+        system_and_poincare_surface_for_closed_orbit_integration(sys,
+                                                                 first_point);
 
-      auto orbit1 = make_ParticleOrbit(my_system_and_pc, first_point, max_time, integrationOptions);
+      auto orbit1 = make_ParticleOrbit(my_system_and_pc,
+                                       first_point,
+                                       max_time,
+                                       integrationOptions);
 
       auto orbit_range = orbit1.range();
 
       auto surface_fun = [surf = my_system_and_pc.poincare_surface()] (auto s)
       { return surf.eval(s); };
 
-      auto close_enough_to_initial_point = [initial_point = first_point, options = integrationOptions] (const auto& s)
+      auto close_enough_to_initial_point =
+        [initial_point = first_point,
+         options = integrationOptions] (const auto& s)
       {
-          const auto p_init = initial_point[static_cast<unsigned>(CoordinateTag::p)];
+          const auto p_init =
+            initial_point[static_cast<unsigned>(CoordinateTag::p)];
+
           const auto p_current = s[static_cast<unsigned>(CoordinateTag::p)];
+
           return std::abs(p_current - p_init) < options.abs_err * 100;
       };
 
@@ -299,12 +366,17 @@ namespace
 
       while (true)
         {
-          range_it = PanosUtilities::copy_until_zero_cross_transformed(range_it,
-                                                                       end_range,
-                                                                       std::back_inserter(outputContainer),
-                                                                       surface_fun,
-                                                                       1.0,
-                                                                       my_system_and_pc.poincare_surface().direction);
+          const auto poinc_surf_direction =
+            my_system_and_pc.poincare_surface().direction;
+
+          range_it =
+            PanosUtilities::copy_until_zero_cross_transformed(
+                                           range_it,
+                                           end_range,
+                                           std::back_inserter(outputContainer),
+                                           surface_fun,
+                                           1.0,
+                                           poinc_surf_direction);
 
           if (range_it == orbit_range.end())
             {
@@ -314,7 +386,8 @@ namespace
 
           else
             {
-              const auto refined_point = accurate_from_rough_cross_point(my_system_and_pc, *range_it);
+              const auto refined_point =
+                accurate_from_rough_cross_point(my_system_and_pc, *range_it);
 
               if (close_enough_to_initial_point(refined_point))
                 {
@@ -329,6 +402,7 @@ namespace
     }
 }
 
+
 template<typename System>
 std::vector<typename System::StateType>
 integrate_along_closed_orbit (System sys,
@@ -338,9 +412,15 @@ integrate_along_closed_orbit (System sys,
 {
 
   std::vector<typename System::StateType> output{};
-  integrate_along_closed_orbit_impl(sys, first_point, output, max_time, integrationOptions);
+  integrate_along_closed_orbit_impl(sys,
+                                    first_point,
+                                    output,
+                                    max_time,
+                                    integrationOptions);
+
   return output;
 }
+
 
 template<typename System>
 typename System::StateType
@@ -351,7 +431,11 @@ last_point_on_closed_orbit (System sys,
 {
 
   BlackHoleContainer<typename System::StateType> no_output{};
-  return integrate_along_closed_orbit_impl(sys, first_point, no_output, max_time, integrationOptions);
+  return integrate_along_closed_orbit_impl(sys,
+                                           first_point,
+                                           no_output,
+                                           max_time,
+                                           integrationOptions);
 }
 
 #endif //ODE_INTEGRATORS_INTEGRATION_UTILITIES_HPP
