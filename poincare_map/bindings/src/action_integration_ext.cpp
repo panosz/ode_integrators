8fd428ae78ed6e4bf8b151c1eb537a98fa28cc55
Panagiotis Zestanakis <panosz@gmail.com>
2019-10-21 18:47:45 +0300

    Bindings. removed deprecated funtions
    -- removed functionality that has been encapsulated by the DynamicSystem classes
    -- The functions that where removed are:
        integrate_E_H_O
        integrate_E_Pendulum
        closed_harmonic_osc_orbit
        closed_pendulum_orbit

--- poincare_map/bindings/src/action_integration_ext.cpp
+++ poincare_map/bindings/src/action_integration_ext.cpp
@@ -3,10 +3,9 @@
 #include <string>
 #include "hamiltonian_dynamic_system.hpp"
 #include "state_bindings.hpp"
-#include "action_integration_bindings.hpp"
 #include "integration_options_bindings.hpp"
-#include "orbit_integration_bindings.hpp"
 #include "dynamic_system_bindings.hpp"
+#include "ActionIntegrationResultDecorator.hpp"
 
 namespace p = boost::python;
 namespace np = boost::python::numpy;
@@ -20,12 +19,8 @@ BOOST_PYTHON_MODULE(action_integration_ext)
   export_args_to_Hessian();
   IntegrationOptionsBindings::export_IntegrationOptions();
   ActionIntegrationBindings::export_ActionIntegrationResultDecorator();
-  ActionIntegrationBindings::export_integrate_E_H_O();
-  ActionIntegrationBindings::export_integrate_E_Pendulum();
   StateBindings::ArmaSB::export_iterable_to_ndarray_for_testing();
 
-  OrbitIntegrationBindings::export_closed_pendulum_orbit();
-  OrbitIntegrationBindings::export_closed_harmonic_osc_orbit();
   DynamicSystemBindings::export_pendulum_dynamic_system();
   DynamicSystemBindings::export_harmonic_osc_dynamic_system();
 
