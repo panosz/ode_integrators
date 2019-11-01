1108c207f303c53376e4e7fd56695dd8d349d9a9
Panagiotis Zestanakis <panosz@gmail.com>
2019-11-01 14:24:48 +0200

    Explicit template instantiation
    -- changes apply to:
          UnperturbedExtendedPendulumHamiltonian

--- poincare_map/include/hamiltonian_dynamic_system.hpp
+++ poincare_map/include/hamiltonian_dynamic_system.hpp
@@ -58,82 +58,24 @@ namespace DS
       double M_;
 
      public:
-      using StateType=PhaseSpaceState;
-
       explicit UnperturbedExtendedPendulumHamiltonian (double M)
           : M_{M}
       { };
 
       template<typename ST>
-      double value(const ST& s) const noexcept
-      {
-        const auto& p = s[static_cast<unsigned>(CoordinateTag::p)];
-        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
-        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
-        return M_ * p * p / 2 - F * cos(q);
-      }
+      double value(const ST& s) const noexcept;
 
       template<typename ST>
-      double operator() (const ST& s) const
-      {
-        return value(s);
-      }
+      double operator() (const ST& s) const;
 
       template<typename ST>
-      FirstDerivatives first_derivatives (const ST& s) const noexcept
-      {
-        FirstDerivatives derivs{};
-        const auto& p = s[static_cast<unsigned>(CoordinateTag::p)];
-        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
-        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
-
-        derivs.dp = M_ * p;
-        derivs.dq = F * sin(q);
-        derivs.dF = -cos(q);
-        return derivs;
-      }
+      FirstDerivatives first_derivatives (const ST& s) const noexcept;
 
       template<typename ST>
-      SecondDerivatives second_derivatives (const ST& s) const noexcept
-      {
-        SecondDerivatives second_derivs{};
-
-        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
-        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
-
-        second_derivs.dp2 = M_;
-        second_derivs.dp_dq = 0;
-        second_derivs.dp_dF = 0;
-        second_derivs.dq2 = F * cos(q);
-        second_derivs.dq_dF = sin(q);
-        second_derivs.dF2 = 0;
-
-        return second_derivs;
-      }
+      SecondDerivatives second_derivatives (const ST& s) const noexcept;
 
       template<typename ST>
-      ThirdDerivatives third_derivatives (const ST& s) const noexcept
-      {
-        ThirdDerivatives third_derivs{};
-
-        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
-        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
-
-        third_derivs.dp3 = 0;
-        third_derivs.dp2_dq = 0;
-        third_derivs.dp2_dF = 0;
-        third_derivs.dp_dq2 = 0;
-        third_derivs.dp_dq_dF = 0;
-        third_derivs.dp_dF2 = 0;
-
-        third_derivs.dq3 = -F * sin(q);
-        third_derivs.dq2_dF = cos(q);
-        third_derivs.dq_dF2 = 0;
-
-        third_derivs.dF3 = 0;
-
-        return third_derivs;
-      }
+      ThirdDerivatives third_derivatives (const ST& s) const noexcept;
 
     };
 
--- poincare_map/src/hamiltonian_dynamic_system.cpp
+++ poincare_map/src/hamiltonian_dynamic_system.cpp
@@ -7,36 +7,41 @@
 namespace DS
 {
 
-    using UE_OH = UnperturbedExtendedOscillatorHamiltonian;
+    using UE_O = UnperturbedExtendedOscillatorHamiltonian;
+    using UE_P  = UnperturbedExtendedPendulumHamiltonian;
     using P_S = PhaseSpaceState;
     using E_S = ExtendedSpaceState;
 
 
+    /**********************************************
+    *  UnperturbedExtendedOscillatorHamiltonian  *
+    **********************************************/
+
 
       template<typename ST>
-      double UE_OH::value(const ST& s) const noexcept
+      double UE_O::value(const ST& s) const noexcept
       {
         const auto& p = s[static_cast<unsigned>(CoordinateTag::p)];
         const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
         const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
         return M_ * p * p  + F * q * q;
       }
-      template double UE_OH::value<P_S>(const P_S&) const noexcept;
-      template double UE_OH::value<E_S>(const E_S&) const noexcept;
+      template double UE_O::value<P_S>(const P_S&) const noexcept;
+      template double UE_O::value<E_S>(const E_S&) const noexcept;
 
 
 
       template<typename ST>
-      double  UE_OH::operator() (const ST& s) const noexcept
+      double  UE_O::operator() (const ST& s) const noexcept
       {
         return value(s);
       }
-      template double  UE_OH::operator()(const P_S& ) const noexcept;
-      template double  UE_OH::operator()(const E_S& ) const noexcept;
+      template double  UE_O::operator()(const P_S& ) const noexcept;
+      template double  UE_O::operator()(const E_S& ) const noexcept;
 
 
       template<typename ST>
-      FirstDerivatives UE_OH::first_derivatives (const ST& s) const noexcept
+      FirstDerivatives UE_O::first_derivatives (const ST& s) const noexcept
       {
         FirstDerivatives derivs{};
         const auto& p = s[static_cast<unsigned>(CoordinateTag::p)];
@@ -48,12 +53,12 @@ namespace DS
         derivs.dF =  q * q;
         return derivs;
       }
-      template FirstDerivatives UE_OH::first_derivatives(const P_S& s) const noexcept;
-      template FirstDerivatives UE_OH::first_derivatives(const E_S& s) const noexcept;
+      template FirstDerivatives UE_O::first_derivatives(const P_S& s) const noexcept;
+      template FirstDerivatives UE_O::first_derivatives(const E_S& s) const noexcept;
 
 
       template<typename ST>
-      SecondDerivatives UE_OH::second_derivatives(const ST& s) const noexcept
+      SecondDerivatives UE_O::second_derivatives(const ST& s) const noexcept
       {
         SecondDerivatives second_derivs{};
 
@@ -69,12 +74,12 @@ namespace DS
 
         return second_derivs;
       }
-      template SecondDerivatives UE_OH::second_derivatives(const P_S&) const noexcept;
-      template SecondDerivatives UE_OH::second_derivatives(const E_S&) const noexcept;
+      template SecondDerivatives UE_O::second_derivatives(const P_S&) const noexcept;
+      template SecondDerivatives UE_O::second_derivatives(const E_S&) const noexcept;
 
 
       template<typename ST>
-      ThirdDerivatives UE_OH::third_derivatives(const ST& /*s*/) const noexcept
+      ThirdDerivatives UE_O::third_derivatives(const ST& /*s*/) const noexcept
       {
         ThirdDerivatives third_derivs{};
 
@@ -94,19 +99,19 @@ namespace DS
 
         return third_derivs;
       }
-      template ThirdDerivatives UE_OH::third_derivatives(const P_S&) const noexcept;
-      template ThirdDerivatives UE_OH::third_derivatives(const E_S&) const noexcept;
+      template ThirdDerivatives UE_O::third_derivatives(const P_S&) const noexcept;
+      template ThirdDerivatives UE_O::third_derivatives(const E_S&) const noexcept;
 
 
 
-    double UE_OH::action
+    double UE_O::action
       (const P_S& s) const
     {
       const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
       return value(s) / (2 * std::sqrt(M_ * F));
     }
 
-    double UE_OH::dKdJ
+    double UE_O::dKdJ
       (const P_S& s) const noexcept
     {
       const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
@@ -114,26 +119,26 @@ namespace DS
       return 2 * std::sqrt(M_ * F);
     }
 
-    double UE_OH::dKdF (const P_S& s) const
+    double UE_O::dKdF (const P_S& s) const
     {
       const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
 
       return action(s) * std::sqrt(M_ / F);
     }
 
-    double UE_OH::d2KdJ2 (const P_S&) const noexcept
+    double UE_O::d2KdJ2 (const P_S&) const noexcept
     {
       return 0;
     }
 
-    double UE_OH::d2KdJdF (const P_S& s) const
+    double UE_O::d2KdJdF (const P_S& s) const
     {
       const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
 
       return std::sqrt(M_ / F);
     }
 
-    double UE_OH::d2KdF2 (const P_S& s) const
+    double UE_O::d2KdF2 (const P_S& s) const
     {
       const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
 
@@ -142,4 +147,90 @@ namespace DS
       return -J * std::sqrt(M_ / F) / (2 * F);
     }
 
+    /********************************************
+    *  UnperturbedExtendedPendulumHamiltonian  *
+    ********************************************/
+
+      template<typename ST>
+      double UE_P::value(const ST& s) const noexcept
+      {
+        const auto& p = s[static_cast<unsigned>(CoordinateTag::p)];
+        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
+        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
+        return M_ * p * p / 2 - F * cos(q);
+      }
+      template double UE_P::value<P_S>(const P_S& s) const noexcept;
+      template double UE_P::value<E_S>(const E_S& s) const noexcept;
+
+      template<typename ST>
+      double UE_P::operator() (const ST& s) const
+      {
+        return value(s);
+      }
+      template double UE_P::operator() (const P_S& s) const;
+      template double UE_P::operator() (const E_S& s) const;
+
+      template<typename ST>
+      FirstDerivatives UE_P::first_derivatives (const ST& s) const noexcept
+      {
+        FirstDerivatives derivs{};
+        const auto& p = s[static_cast<unsigned>(CoordinateTag::p)];
+        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
+        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
+
+        derivs.dp = M_ * p;
+        derivs.dq = F * sin(q);
+        derivs.dF = -cos(q);
+        return derivs;
+      }
+      template FirstDerivatives UE_P::first_derivatives(const P_S& s) const noexcept;
+      template FirstDerivatives UE_P::first_derivatives(const E_S& s) const noexcept;
+
+      template<typename ST>
+      SecondDerivatives UE_P::second_derivatives (const ST& s) const noexcept
+      {
+        SecondDerivatives second_derivs{};
+
+        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
+        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
+
+        second_derivs.dp2 = M_;
+        second_derivs.dp_dq = 0;
+        second_derivs.dp_dF = 0;
+        second_derivs.dq2 = F * cos(q);
+        second_derivs.dq_dF = sin(q);
+        second_derivs.dF2 = 0;
+
+        return second_derivs;
+      }
+      template SecondDerivatives UE_P::second_derivatives(const P_S& s) const noexcept;
+      template SecondDerivatives UE_P::second_derivatives(const E_S& s) const noexcept;
+
+
+      template<typename ST>
+      ThirdDerivatives UE_P::third_derivatives (const ST& s) const noexcept
+      {
+        ThirdDerivatives third_derivs{};
+
+        const auto& q = s[static_cast<unsigned>(CoordinateTag::q)];
+        const auto& F = s[static_cast<unsigned>(CoordinateTag::F)];
+
+        third_derivs.dp3 = 0;
+        third_derivs.dp2_dq = 0;
+        third_derivs.dp2_dF = 0;
+        third_derivs.dp_dq2 = 0;
+        third_derivs.dp_dq_dF = 0;
+        third_derivs.dp_dF2 = 0;
+
+        third_derivs.dq3 = -F * sin(q);
+        third_derivs.dq2_dF = cos(q);
+        third_derivs.dq_dF2 = 0;
+
+        third_derivs.dF3 = 0;
+
+        return third_derivs;
+      }
+
+      template ThirdDerivatives UE_P::third_derivatives(const P_S& s) const noexcept;
+      template ThirdDerivatives UE_P::third_derivatives(const E_S& s) const noexcept;
 }
