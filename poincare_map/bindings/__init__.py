a41fb859a97c5c6937d013057ffc356bf22b0180
Panagiotis Zestanakis <panosz@gmail.com>
2019-12-10 16:13:54 +0200

    python: changed package structure (provisionally)
    —  package name dynamic_analysis with a single module named core
    —  tests do not work (still use the old import commands)

--- poincare_map/bindings/__init__.py
+++
@@ -1,2 +0,0 @@
-
-from . import action_integration_ext
--- poincare_map/bindings/setup.py
+++ poincare_map/bindings/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-setup(name="action_integragion_ext",
-      packages=[''],
-      package_dir={'': '.'},
-      package_data={'': ['action_integragion_ext.so']})
+setup(name="dynamic_analysis",
+      packages=['core'],
+      package_data={'core': ['_core.so']})
