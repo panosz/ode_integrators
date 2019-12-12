ddf945aed4780630574c037c2f6de5621d0b70d8
Panagiotis Zestanakis <panosz@gmail.com>
2019-12-12 14:33:04 +0200

    Imprv: Python. Implemented IntegrationOptions __str__ and __repr__

--- poincare_map/python/dynamic_analysis/core/__init__.py
+++ poincare_map/python/dynamic_analysis/core/__init__.py
@@ -1,1 +1,35 @@
 from dynamic_analysis.core._core import *
+
+
+def _integration_options_details(options):
+    """
+    Get options details in a nice format.
+
+    Returns
+        class_name: str
+        details: dict of attribute - value pairs
+    """
+    class_name = type(options).__name__
+    details = dict([('abs_err', options.abs_err),
+                    ('rel_err', options.rel_err),
+                    ('dt', options.dt),
+                    ('coefficient', options.coefficient)])
+    return class_name, details
+
+
+def _integration_options_str(self):
+    class_name, details = _integration_options_details(self)
+    details_strings = [f'{name}: {value}' for name, value in details.items()]
+    self_strings = [f'{class_name} object:'] + details_strings
+    return '\n\t'.join(self_strings)
+
+
+def _integration_options_repr(self):
+    class_name, details = _integration_options_details(self)
+    arg_string = ', '.join(f'{name}={value}'
+                           for name, value in details.items())
+    return class_name + '(' + arg_string + ')'
+
+
+IntegrationOptions.__str__ = _integration_options_str
+IntegrationOptions.__repr__ = _integration_options_repr
